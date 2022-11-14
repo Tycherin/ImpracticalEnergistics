3582292bd681b800a5b7f43a5f464bc8e1919abe
Tycherin <a@example.com>
2022-11-14 12:19:18 -0800

    Implemented datagen recipes for Rift Catalyst stuff

--- src/main/java/com/tycherin/impen/datagen/RiftCatalystRecipeProvider.java
+++ src/main/java/com/tycherin/impen/datagen/RiftCatalystRecipeProvider.java
@@ -1,8 +1,10 @@
 package com.tycherin.impen.datagen;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -17,6 +19,8 @@ import com.tycherin.impen.logic.rift.RiftWeight;
 import com.tycherin.impen.recipe.RiftCatalystRecipeSerializer;
 import com.tycherin.impen.util.ImpenIdUtil;
 
+import appeng.core.definitions.AEBlocks;
+import appeng.core.definitions.AEItems;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -25,9 +29,13 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.ForgeRegistryEntry;
+import net.minecraftforge.registries.tags.ITag;
 
 public class RiftCatalystRecipeProvider {
-    
+
     public static final String RIFT_CATALYST_NAME = "rift_catalyst";
     public static final Map<ItemDefinition, Block> CATALYSTS_TO_BLOCKS = ImmutableMap.<ItemDefinition, Block>builder()
             .put(ImpenRegistry.RIFT_CATALYST_BLACKSTONE, Blocks.BLACKSTONE)
@@ -38,33 +46,142 @@ public class RiftCatalystRecipeProvider {
             .put(ImpenRegistry.RIFT_CATALYST_STONE, Blocks.STONE)
             .build();
 
+    private static final int BASE_PROBABILITY = 5;
+
     public RiftCatalystRecipeProvider() {
     }
-    
+
     public void addRecipes(final Consumer<FinishedRecipe> consumer) {
-        RiftCatalystRecipeBuilder.of(ImpenRegistry.RIFT_CATALYST_STONE)
-            .consumedItems(Items.APPLE)
-            .weights(new RiftWeight(Blocks.IRON_ORE, 5))
-            .save(consumer, "stone_apple");
-        
-        // TODO Generate remaining recipes
+        final BuilderHelper helper = new BuilderHelper(consumer);
+
+        // Vanilla ores
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_STONE, Blocks.IRON_ORE, Items.IRON_INGOT);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_STONE, Blocks.COPPER_ORE, Items.COPPER_INGOT);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_STONE, Blocks.COAL_ORE, Items.COAL);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DEEPSLATE, Blocks.DEEPSLATE_DIAMOND_ORE, Items.DIAMOND);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DEEPSLATE, Blocks.DEEPSLATE_EMERALD_ORE, Items.EMERALD);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DEEPSLATE, Blocks.DEEPSLATE_REDSTONE_ORE, Items.REDSTONE);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DEEPSLATE, Blocks.DEEPSLATE_LAPIS_ORE, Items.LAPIS_LAZULI);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_NETHERRACK, Blocks.NETHER_GOLD_ORE, Items.GOLD_NUGGET);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_NETHERRACK, Blocks.NETHER_QUARTZ_ORE, Items.QUARTZ);
+
+        // Special vanilla stuff
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DIRT, Blocks.SLIME_BLOCK);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DIRT, Blocks.CLAY);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DIRT, Blocks.BROWN_MUSHROOM_BLOCK, Items.BROWN_MUSHROOM);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DIRT, Blocks.RED_MUSHROOM_BLOCK, Items.RED_MUSHROOM);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_NETHERRACK, Blocks.GLOWSTONE, Items.GLOWSTONE_DUST);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_NETHERRACK, Blocks.MAGMA_BLOCK);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_NETHERRACK, Blocks.ANCIENT_DEBRIS, Items.NETHERITE_SCRAP);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_END_STONE, Blocks.AMETHYST_BLOCK, Items.AMETHYST_SHARD);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_END_STONE, Blocks.PRISMARINE, Items.PRISMARINE);
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_BLACKSTONE, Blocks.OBSIDIAN);
+
+        // AE2 stuff
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DEEPSLATE, AEBlocks.DEEPSLATE_QUARTZ_ORE.block(),
+                AEItems.CERTUS_QUARTZ_CRYSTAL.asItem());
+        helper.standardBlock(ImpenRegistry.RIFT_CATALYST_BLACKSTONE, AEBlocks.SKY_STONE_BLOCK.block());
+
+        // Catchall for properly tagged ore blocks added by other mods
+        // Note that the order is important - prefer more advanced stone types/catalysts over easier ones
+        ForgeRegistries.BLOCKS.tags().getTag(Tags.Blocks.ORES_IN_GROUND_DEEPSLATE).forEach(ore -> {
+            helper.standardBlock(ImpenRegistry.RIFT_CATALYST_DEEPSLATE, ore);
+        });
+        ForgeRegistries.BLOCKS.tags().getTag(Tags.Blocks.ORES_IN_GROUND_NETHERRACK).forEach(ore -> {
+            helper.standardBlock(ImpenRegistry.RIFT_CATALYST_NETHERRACK, ore);
+        });
+        ForgeRegistries.BLOCKS.tags().getTag(Tags.Blocks.ORES_IN_GROUND_STONE).forEach(ore -> {
+            helper.standardBlock(ImpenRegistry.RIFT_CATALYST_STONE, ore);
+        });
     }
-    
+
+    private static class BuilderHelper {
+        private final Consumer<FinishedRecipe> consumer;
+        private final ITag<Block> denseTag;
+        private final ITag<Block> singularTag;
+        private final ITag<Block> sparseTag;
+
+        private final Set<String> generatedRecipes = new HashSet<>();
+
+        public BuilderHelper(final Consumer<FinishedRecipe> consumer) {
+            this.consumer = consumer;
+            this.denseTag = ForgeRegistries.BLOCKS.tags().getTag(Tags.Blocks.ORE_RATES_DENSE);
+            this.singularTag = ForgeRegistries.BLOCKS.tags().getTag(Tags.Blocks.ORE_RATES_SINGULAR);
+            this.sparseTag = ForgeRegistries.BLOCKS.tags().getTag(Tags.Blocks.ORE_RATES_SPARSE);
+        }
+
+        public void standardBlock(final ItemDefinition catalyst, final Block block) {
+            checkRecipe(block, (recipeName) -> {
+                RiftCatalystRecipeBuilder.of(catalyst)
+                        .consumedItems(block.asItem())
+                        .weights(new RiftWeight(block, BASE_PROBABILITY))
+                        .save(consumer, recipeName);
+            });
+        }
+
+        public void standardBlock(final ItemDefinition catalyst, final Block block, final Item item) {
+            standardBlock(catalyst, block);
+
+            checkRecipe(item, (recipeName) -> {
+                final double exchangeRate;
+                if (sparseTag.contains(block)) {
+                    exchangeRate = 2;
+                }
+                else if (singularTag.contains(block)) {
+                    exchangeRate = 1;
+                }
+                else if (denseTag.contains(block)) {
+                    // TODO It really makes more sense to model this as requiring more of the input item, otherwise this
+                    // is kinda useless
+                    exchangeRate = .25;
+                }
+                else {
+                    // Not an ore, or not a properly tagged ore
+                    exchangeRate = .25;
+                }
+
+                RiftCatalystRecipeBuilder.of(catalyst)
+                        .consumedItems(item)
+                        .weights(new RiftWeight(block, BASE_PROBABILITY * exchangeRate))
+                        .save(consumer, recipeName);
+            });
+        }
+
+        private void checkRecipe(final ForgeRegistryEntry<?> ingredient, final Consumer<String> generateRecipeFunc) {
+            final String ingredientRawName = ingredient.getRegistryName().getPath();
+
+            // We don't want to add extra recipes for ores that have multiple variants, so we strip the variant prefix
+            // off to create the canonical name. This means that whichever recipe gets added first trumps the others.
+            final String recipeName;
+            if (ingredientRawName.startsWith("deepslate_") || ingredientRawName.startsWith("nether_")) {
+                recipeName = ingredientRawName.split("_", 2)[1]; // mmm, regex shenanigans
+            }
+            else {
+                recipeName = ingredientRawName;
+            }
+
+            if (!generatedRecipes.contains(recipeName)) {
+                generateRecipeFunc.accept(recipeName);
+                generatedRecipes.add(recipeName);
+            }
+        }
+    }
+
     public static class RiftCatalystRecipeBuilder {
         private Block baseBlock;
         private Item catalyst;
         private List<Ingredient> consumedItems;
         private List<RiftWeight> weights;
-        
+
         private RiftCatalystRecipeBuilder() {
         }
-        
+
         public static RiftCatalystRecipeBuilder of(final ItemDefinition catalyst) {
             final var builder = new RiftCatalystRecipeBuilder();
             if (!CATALYSTS_TO_BLOCKS.containsKey(catalyst)) {
                 throw new IllegalArgumentException("No mapping found for catalyst: " + catalyst);
             }
-            
+
             builder.catalyst = catalyst.asItem();
             builder.baseBlock = CATALYSTS_TO_BLOCKS.get(catalyst);
             return builder;
@@ -88,7 +205,7 @@ public class RiftCatalystRecipeProvider {
         public RiftCatalystRecipeBuilder consumedItems(final Ingredient... consumedItems) {
             return this.consumedItems(Arrays.asList(consumedItems));
         }
-        
+
         public RiftCatalystRecipeBuilder consumedItems(final Item... consumedItems) {
             return this.consumedItems(
                     Arrays.asList(consumedItems).stream().map(Ingredient::of).collect(Collectors.toList()));
